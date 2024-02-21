58bde8bf9b4778bcf4e5bcdd90ce0eead4d96b4d
Juhhxx <119042560+Juhhxx@users.noreply.github.com>
2024-02-21 12:34:12 +0000

    Added comment

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,6 +2,10 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Prints Hello, world! and Bye World! to the console.
+    /// </summary>
+    /// <param name="args"></param>
     class Program
     {
         static void Main(string[] args)
