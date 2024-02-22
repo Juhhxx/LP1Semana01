61bc94ecbc0a366bfe1ba9fafbe10aa4b3e68486
Julia Costa <juliacosta232005@gmail.com>
2024-02-22 23:59:15 +0000

    Added unicode char exercise

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -6,6 +7,9 @@ namespace VariousTypes
     {
         static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            // Exercício Integers
+
             int x = -100;
             short x1 = 10;
             long x2 = 1_500_000;
@@ -17,6 +21,23 @@ namespace VariousTypes
             Console.WriteLine($"Variável tipo long: {x2}");
             Console.WriteLine($"Variável tipo uint: {ux}");
             Console.WriteLine($"Variável tipo ulong: {ux2}");
+
+            // Exercício Chars
+
+            char fist = '\u270A';
+            char arrow = '\u2934';
+            char c = '\u0063';
+            char star = '\u2605';
+            char hamSick = '\u262D';
+            char square = '\u25A1';
+
+            Console.WriteLine("Boletin de Voto\n");
+            Console.WriteLine($"{square} PS {fist}");
+            Console.WriteLine($"{square} PSD {arrow}");
+            Console.WriteLine($"{square} BE {star}");
+            Console.WriteLine($"{square} PCP {hamSick}");
+            Console.WriteLine($"{square} CHEGA {c}");
+
         }
     }
 }
