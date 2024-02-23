a0da9a2795dde5aff0c826bf87f6831a5cd5a7c4
Julia Costa <juliacosta232005@gmail.com>
2024-02-23 01:14:31 +0000

    Added string formating exercice

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,9 @@ namespace ManyStrings
     {
         static void Main()
         {
+            double xx = 1.23456;
+            int ii = 19;
+
             // Exercices strings
             string s = "Era uma vez\nUma pessoa\nChamada Afonso\n\t \uA231";
             string chessboardW = "\u2656 \u2658 \u2657 \u2654 \u2655 \u2657 \u2658 \u2656";
@@ -25,6 +28,18 @@ namespace ManyStrings
             Console.WriteLine(sc);
             Console.WriteLine(si);
             Console.WriteLine("\n");
+
+            // Exercise formating
+
+            string dec = $"{xx:f2}";
+            string per = $"{xx:p1}";
+            string hex = $"{ii:x}";
+            string cur = $"{ii:c}";
+
+            Console.WriteLine(dec);
+            Console.WriteLine(per);
+            Console.WriteLine(hex);
+            Console.WriteLine(cur);
 
         }
     }
