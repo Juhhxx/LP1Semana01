d5c5b4191dc5ba597b295ef0f94bbfe27aacbdee
Julia Costa <juliacosta232005@gmail.com>
2024-02-23 01:06:05 +0000

    Added string concatenation and interpolation exercice

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,7 @@ namespace ManyStrings
     {
         static void Main()
         {
+            // Exercices strings
             string s = "Era uma vez\nUma pessoa\nChamada Afonso\n\t \uA231";
             string chessboardW = "\u2656 \u2658 \u2657 \u2654 \u2655 \u2657 \u2658 \u2656";
             char pondW = '\u2659';
@@ -14,6 +15,15 @@ namespace ManyStrings
             Console.WriteLine(s);
             Console.WriteLine(chessboardW);
             Console.WriteLine(chessboardW2);
+            Console.WriteLine("\n");
+
+            // Exercices strings 2
+            int two = 2;
+            string sc = "Tinha um (" + 1 + ") dedo.";
+            string si = $"E dois ({two}) pés.";
+
+            Console.WriteLine(sc);
+            Console.WriteLine(si);
             Console.WriteLine("\n");
 
         }
