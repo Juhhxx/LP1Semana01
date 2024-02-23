a2d10a9f9ff5ab3da3cf5fc058237ac2c7663e09
Julia Costa <juliacosta232005@gmail.com>
2024-02-23 00:51:48 +0000

    Added string exercices

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,7 +6,16 @@ namespace ManyStrings
     {
         static void Main()
         {
-            
+            string s = "Era uma vez\nUma pessoa\nChamada Afonso\n\t \uA231";
+            string chessboardW = "\u2656 \u2658 \u2657 \u2654 \u2655 \u2657 \u2658 \u2656";
+            char pondW = '\u2659';
+            string chessboardW2 = $"{pondW} {pondW} {pondW} {pondW} {pondW} {pondW} {pondW} {pondW}";
+  
+            Console.WriteLine(s);
+            Console.WriteLine(chessboardW);
+            Console.WriteLine(chessboardW2);
+            Console.WriteLine("\n");
+
         }
     }
 }
